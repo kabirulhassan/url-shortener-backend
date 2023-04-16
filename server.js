621131eeab0e2a91d8761b8e4b2affd4f2154a7c
kabirulhassan <kabirulhassan7@gmail.com>
2023-04-17 04:58:26 +0530

    add cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 
 connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json()); // body parser middleware
 
